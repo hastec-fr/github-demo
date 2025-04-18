33b32c956206aa47c7d90a416473a87e6c8ad994
Benjamin Vargin <benjamin.vargin.pro@proton.me>
2025-04-18 13:49:40 +0200

    Initial commit

---
+++ cypress/e2e/todo.cy.ts
@@ -0,0 +1,14 @@
+describe('Todo List Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/todo');
+  });
+
+  it('should add a task', () => {
+    cy.get('input').type('My New Task');
+    cy.contains('Add').click();
+    cy.get('[name=task-field]').type('Another Task{enter}');
+    cy.get('.task-list tr').should('have.length', 2);
+  });
+
+});
+
