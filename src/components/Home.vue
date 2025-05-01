379eb7b7a3126bfae7fcde2bce32005b657b39a3
Benjamin Vargin <benjamin.vargin.pro@proton.me>
2025-05-01 17:08:12 +0200

    Concurrency

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-center my-5" style="color:blueviolet">Welcome</h1>
+  <h1 class="text-center my-5" style="color:chocolate">Welcome</h1>
 </template>
 
 <script setup lang="ts">
