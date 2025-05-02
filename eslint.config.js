eca6aa62732604b46d247302e82f0854a57d9dec
Benjamin Vargin <benjamin.vargin.pro@proton.me>
2025-05-02 14:49:18 +0200

    Javascript action (Fix)

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,11 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 
 export default defineConfig([
+  globalIgnores([".github/actions/dummy-js"]),
   { files: ["**/*.{js,mjs,cjs,ts,vue}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,vue}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
@@ -17,5 +18,5 @@ export default defineConfig([
           "ignores": ["Home"]
        }],
     }
-  }
+  },
 ]);
